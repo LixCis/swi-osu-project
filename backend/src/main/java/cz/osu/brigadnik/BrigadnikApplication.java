85d239d31bab3c9f30b33a97963d624c5b9dd0b5
Radim Janko <58134759+LixCis@users.noreply.github.com>
2026-04-13 13:12:20 +0200

    Initial backend setup with Spring Boot and MariaDB

---
+++ backend/src/main/java/cz/osu/brigadnik/BrigadnikApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.brigadnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrigadnikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrigadnikApplication.class, args);
+    }
+
+}
