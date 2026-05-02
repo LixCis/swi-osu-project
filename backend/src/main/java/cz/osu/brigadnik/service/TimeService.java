5681f19aac5dfe1198e2dec419a43af319ba82c6
Radim Janko <58134759+LixCis@users.noreply.github.com>
2026-05-02 16:39:31 +0200

    Add missing Comparator import to TimeService

--- backend/src/main/java/cz/osu/brigadnik/service/TimeService.java
+++ backend/src/main/java/cz/osu/brigadnik/service/TimeService.java
@@ -23,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
